59a51620750c4afd8394971eeec27cbda61fff1c
Sahiti Gopal <sahitigopal20@gmail.com>
2015-12-02 12:35:46 +0530

    Add existing members to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @members = @group.users
     @group_membership = @group.memberships.new
   end
 
