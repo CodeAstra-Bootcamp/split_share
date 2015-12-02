dc97de8c29aaede97a900b932633f972d8011136
Sahiti Gopal <sahitigopal20@gmail.com>
2015-12-02 12:54:09 +0530

    Add admin to the group by default

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.admin = current_user
     if @group.save
-      redirect_to groups_path
+      redirect_to @group
     else
       render :new
     end
